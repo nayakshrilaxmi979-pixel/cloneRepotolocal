933733ac37ec113d8c9266b03771da9bbd465269
nayakshrilaxmi979-pixel <nayakshrilaxmi979@gmail.com>
2026-09-02 14:54:22 +0530

    create userdetails.py

--- userdetails.py
+++ userdetails.py
@@ -1,8 +1,6 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
-city = input("Enter your city")
-age = input("Enter your age")
+
 
 print("User first name and last name",firstname+" "+lastname)
-print("User city:",city")
-print("User age:",age")
+
