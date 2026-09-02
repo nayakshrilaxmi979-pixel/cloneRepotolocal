a1303c0a72d23cb4658cf86b2f8c466b53a0b7d0
Shrilaxmi <nayakshrilaxmi979@gmail.com>
2026-09-02 15:10:38 +0530

    save

--- userdetails1.py
+++ userdetails1.py
@@ -1,5 +1,6 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
-
+age = input("Enter your age")
 
 print("User first name and last name",firstname+" "+lastname)
+print("User age:",age)
