4f275f3e27812c3724eab3426ffbe7d95dafbf79
nayakshrilaxmi979-pixel <nayakshrilaxmi979@gmail.com>
2026-09-02 15:12:28 +0530

    Update userdetails1.py

--- userdetails1.py
+++ userdetails1.py
@@ -1,6 +1,8 @@
 firstname = input("Enter your first name")
 lastname = input("Enter your last name")
 age = input("Enter your age")
-
+contact = input("Enter your contact")
 print("User first name and last name",firstname+" "+lastname)
 print("User age:",age)
+print("User contact:",contact)
+
